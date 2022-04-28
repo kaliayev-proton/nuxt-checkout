f2e6fec4a5ccfe5a4a207a6131fda18d1fa53cbd
Víctor Várez <kaliayev@protonmail.com>
2022-04-28 21:42:24 +0200

    first commit

---
+++ pages/Error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <main>
+      <div class="py-5 text-center">
+        <h2>Welcome</h2>
+        <p class="lead">has invited you to buy this products!</p>
+      </div>
+    </main>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
